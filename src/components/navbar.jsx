6548d6ab6f0627759487d08edc0921b2bc71b78d
Kazak Anastasiia <akazak009@gmail.com>
2022-10-17 22:43:59 +0300

    final

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div>
+            <td><a href="/menu">Menu</a></td>
+        </div>
+    );
+};
+
+export default NavBar;
